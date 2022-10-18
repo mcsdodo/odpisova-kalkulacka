c251131bc894ea82686c7814333e31bef76a0634
Jozef Lačný <lacny.jozef@gmail.com>
2022-10-18 12:50:41 +0200

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-3">
+    <Container className="p-5 mb-4 bg-light rounded-3">
+      <h1 className="header">Welcome To React-Bootstrap</h1>
+      {/* <ExampleToast> */}
+        We now have Toasts
+        <span role="img" aria-label="tada">
+          🎉
+        </span>
+      {/* </ExampleToast> */}
+    </Container>
+  </Container>
   );
 }
 
